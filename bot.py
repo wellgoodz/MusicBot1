4b876f34c5e43798ce8b9f423fe81c11f77fd91c
wellgoodz <49289072+wellgoodz@users.noreply.github.com>
2019-04-05 00:23:13 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,10 @@
+import discord
 from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncin
+import random
+import requests
+import os
 
 client = commands.Bot(command_prefix="!")
 player_dict = dict()

2e660e08c46d33dbfdc997f4fb8b33606374d993
wellgoodz <49289072+wellgoodz@users.noreply.github.com>
2019-04-06 13:28:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
-import asyncin
+import asyncio
 import random
 import requests
 import os
@@ -12,24 +12,47 @@ player_dict = dict()
 
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print("Bot ist bereit")
+
 
 
 @client.command(pass_context=True)
-async def play(ctx, url):
-    channel = ctx.message.author.voice.voice_channel
+async def play (ctx, url):
+    channel = ctx.message.author.voice_channel
     await client.join_voice_channel(channel)
     server = ctx.message.server
     voice = client.voice_client_in(server)
     player = await voice.create_ytdl_player(url)
     player_dict[server.id] = player
+    await client.send_message(ctx.message.channel, "Playing %s now" % player.title)
     player.start()
+
 
 @client.command(pass_context=True)
 async def stop(ctx):
     server = ctx.message.server
     player = player_dict[server.id]
     player.stop()
+    await client.send_message(ctx.message.channel, "Stopped %s" % player.title)
     del player_dict[server.id]
-    
-client.run(str(os.eviron.get("BOT_TOKEN")))
+
+
+@client.command(pass_context=True)
+async def pause(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.pause()
+    await client.send_message(ctx.message.channel, "Paused %s" % player.title)
+
+
+@client.command(pass_context=True)
+async def resume(ctx):
+    server = ctx.message.server
+    player = player_dict[server.id]
+    player.resume()
+    await client.send_message(ctx.message.channel, "Resumed %s" % player.title)
+
+
+
+
+client.run(str(os.environ.get('BOT_TOKEN')))
